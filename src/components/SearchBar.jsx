202c55338ce20440ab0748372024bb6843da64c3
giannivitale <giavitale78@gmail.com>
2025-11-19 13:59:33 +0100

    create SearchBar component with initial state and input handling

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const SearchBar = ({ onSearch}) => {
+    const[query, setQuery] = useState();
+
+    const handleInputChange = (e) => {
+        setQuery(e.trget.value);
+    };
+
+    const handleSearch = () => {
+        if(?????) {
+           // ?????
+        } 
+    };
+
+    return (
+
+
+        // ??????????????????
+
+
+    )
+
+}
