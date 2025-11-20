3ae8298fe0c7739f6c3ae18c8d0fd508990862d8
giannivitale <giavitale78@gmail.com>
2025-11-20 11:07:26 +0100

    refactor App component and implement SearchBar with input handling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,15 +8,23 @@ const SearchBar = ({ onSearch}) => {
     };
 
     const handleSearch = () => {
-        if(?????) {
-           // ?????
+        if(onSearch) {
+           onSearch(query);
         } 
     };
 
     return (
 
+        <div className="">
+            <input type="text" 
+            className=""
+            placeholder="Cerca un film.."
+            value={query}
+            onChange={handleInputChange}
+            />
+            <button className="" onClick={handleSearch}></button>
+        </div>
 
-        // ??????????????????
 
 
     )
