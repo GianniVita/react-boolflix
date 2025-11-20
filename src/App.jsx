80a92e1cfa17e22d81a704534362e64bd3de0314
giannivitale <giavitale78@gmail.com>
2025-11-20 12:34:47 +0100

    implement API call in App component using axios and log response data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react"
-import SearchBar from "./components/SearchBar"
+import { useEffect } from "react";
+import axios from "axios";
+
 
 
 function App() {
 
-  const api_endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_MOVIE_DB_API_KEY}&${}`
+  //const api_endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_MOVIE_DB_API_KEY}&${}`
 
- 
+  const api_endpoint = `https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro`
+
+useEffect(() => {
+  axios.get(api_endpoint)
+    .then((response) =>{
+      console.log(response.data);
+      
+    });
+
+}, []); 
 
   return (
     <>
@@ -19,7 +30,7 @@ function App() {
       </div>
       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
